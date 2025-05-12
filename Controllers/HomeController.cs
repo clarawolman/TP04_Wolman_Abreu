542872e0f95dddf06cb6684021e52c6b96cbd120
CECILIA ABREU <49433586@ort>
2025-05-12 09:00:25 -0300

    Avance

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,4 +17,8 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    public IActionResult Jugar() {
+        ViewBag.LetrasPalabra = Ahorcado.HacerAhorcado();
+    }
 }
--- Models/Ahorcado.cs
+++ Models/Ahorcado.cs
@@ -3,26 +3,35 @@ public class Ahorcado {
     public List<char> LetrasArriesgadas{get; private set;}
     public List<char> LetrasPalabra{get; private set;}
     public string PalabraArriesgada{get; private set;}
+    public char LetraArriesgada{get; private set;}
 
     public Ahorcado(string palabra, List<char> letraArriesgada, string palabraArriesgada) {
         Palabra = palabra;
         LetrasArriesgadas = letraArriesgada;
         PalabraArriesgada = palabraArriesgada;
     }
-    public void PalabraElegida() {
+    // hacer la palabra apta ahorcado
+    public void HacerAhorcado() {
         int i = 0;
         foreach (Char letra in Palabra) {
-            LetrasPalabra[i] = letra;
+            LetrasPalabra[i] = '_';
             i++;
         }
     }
-    public char ArriesgarLetra(char LetraArriesgada) {
-        int i = 0;
-        foreach (Char letra in LetrasPalabra) {
-            if (LetraArriesgada == letra){
-                LetrasPalabra[i] = LetraArriesgada;
+    // para ver que letras arriesgo
+    public void ArriesgarLetra(char LetraUsuario) {
+        foreach (Char letra in Palabra) { 
+            if (LetraUsuario == letra){
+                LetrasPalabra[LetrasPalabra.IndexOf(LetraUsuario)] = LetraUsuario;
             }
-            i++;
+            LetrasArriesgadas.Add(LetraUsuario);
         }
+    }
+    public bool ArriesgarPalabra(string Arriesgada) {
+        bool ganaste = false;
+        if (Arriesgada == Palabra) {
+            ganaste = true;
+        }
+        return ganaste;
     }
 }
